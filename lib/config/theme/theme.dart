69bf884a28369e33e0aee3eca33f6c5c1cce8e79
gquende <gquende@gmail.com>
2024-08-21 22:11:03 +0100

    ✨ Define Colors
    Define COlors

---
+++ lib/config/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppTheme {
+  static Color PRIMARY_COLOR = Color(0xffff0f0f0);
+  static Color BACKGROUND_COLOR = Color(0xff05121A);
+  static Color CONTAINER_COLOR = Color(0xff1F282F);
+  static Color WHITE_GREY_COLOR = Color(0xffDDE4EC);
+}
